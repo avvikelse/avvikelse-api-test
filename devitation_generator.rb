5bf8a5a1c7108b3250da0eb76ff56b301f3c0a9d
Hardy Ferentschik <hardy@ferentschik.de>
2011-10-09 01:23:58 +0200

    something is working :-)

--- devitation_generator.rb
+++ devitation_generator.rb
@@ -5,11 +5,14 @@ require 'json'
 require 'xmlsimple'
 require 'htmlentities'
 
+
+
 # define uri of the deviation api
 class DevitationApi
   include HTTParty
   base_uri 'http://localhost:8888/v1'
 end
+
 
 
 # define uri of vasttrafik stop list
@@ -18,26 +21,57 @@ class GetStopList
   base_uri 'http://vasttrafik.se/External_Services/TravelPlanner.asmx/'
 end
 
+
+
 # container for stop list with abiltiy to retrieve random stop
 class StopList
   def initialize
-    params = {"identifier" => "42b04690-659f-4659-b4b0-16aba2a9ad09"}
-    out = GetStopList.post('/GetAllStops', :body => params)
-    nested_xml = XmlSimple.xml_in(out.response.body)
-    xml = HTMLEntities.new.decode(nested_xml['content'])
-    puts xml
+    # params = {"identifier" => "42b04690-659f-4659-b4b0-16aba2a9ad09"}
+    # out = GetStopList.post('/GetAllStops', :body => params)
+    # nested_xml = XmlSimple.xml_in(out.response.body)
+    # xml = HTMLEntities.new.decode(nested_xml['content'])
+    # puts xml
+    # xml['root']['all_stops']['items']['item'].each do |item|
+    # puts item
+    # end
   end
 end
 
+
+
 class Deviation
-  attr_accessor :comment, :scope, :latitude, :longitude, :transport, :source
+  # needs to be backed up with some "real data" -> vasttrafik!?
+  @@comments = ["olycka", "motor skada", "vet inte vaför", "alltid versenat"]
+  @@lines = [ "1", "2", "3"  ]
+  @@vehicles = [ "Oscar", "Hannah"  ]
+  @@coords = [[ "11.981211", "57.709792"], [ "11.981300", "58.709792"] ]
+  @@transports = ["BUS", "TRAIN", "SUBWAY"]
+
+  attr_accessor :comment, :line, :vehicle, :latitude, :longitude, :transport, :source
+
+  def initialize(randomize)
+    if randomize == true
+    @comment = @@comments.sample
+    @line = @@lines.sample
+    @vehicle = @@vehicles.sample
+    coord = @@coords.sample
+    @latitude = coord[0]
+    @longitude = coord[1]
+    @transport = @@transports.sample
+    @source = "crowd"
+    end
+  end
+
   def to_hash()
     dev = Hash.new
     if @comment != nil
     dev['comment'] = @comment
     end
-    if @scope != nil
-    dev['scope'] = @scope
+    if @line != nil
+    dev['line'] = @line
+    end
+    if @vehicle != nil
+    dev['vehicle'] = @vehicle
     end
     if @latitude != nil
     dev['latitude'] = @latitude
@@ -58,13 +92,16 @@ end
 
 
 class DeviationGenerator
-  def initialize(api)
+  def initialize(api, interval = '5s')
     @scheduler = Rufus::Scheduler.start_new
     @api = api
+    @interval = interval
   end
 
-  def createDeviation(params)
-    @scheduler.every '2s' do
+  def createRandomDeviation
+    @scheduler.every @interval do
+      dev = Deviation.new(true)
+      params = dev.to_hash
       puts 'Creating new deviation ' << params.to_s
       out = DevitationApi.post('/deviations/', :body => params)
       id = JSON.parse( out.response.body )['id']
@@ -74,9 +111,7 @@ class DeviationGenerator
   end
 end
 
+# put things together
 api = DevitationApi.new
-StopList.new
 generator = DeviationGenerator.new(api)
-#dev = Deviation.new
-#dev.comment = "hello"
-#generator.createDeviation(dev.to_hash)
+generator.createRandomDeviation()
