80a39dafdfb8ee37b7901fa49a9be114fea42fdd
Hardy Ferentschik <hardy@ferentschik.de>
2011-10-08 17:23:29 +0200

    added test against localhost

--- spec/happy_case_spec.rb
+++ spec/happy_case_spec.rb
@@ -2,17 +2,18 @@
 require 'httparty'
 require 'json'
 
-API_URL = "http://94.247.169.81"
+API_URL = "http://localhost:8888/v1/deviation"
 
 describe "avvikelse-api" do
+ 
+  # Helper functions to build requests
   def post value
-    #HTTParty.post "#{API_URL}/V1/", {:body => value}
-    @value = value
+    HTTParty.post "#{API_URL}", {:body => value}
   end
 
-  def get query
-    #HTTParty.get "#{API_URL}/V1/query"
-    [JSON.parse( @value ) ]
+  def get_deviation id
+      out = HTTParty.get "#{API_URL}/#{id}/"
+      JSON.parse( out.response.body ) 
   end
   
   def json_from lat, lon
@@ -24,14 +25,20 @@ describe "avvikelse-api" do
     post report
   end
   
-  def search lat, lon
-    q = json_from lat, lon
-    get q
+  def search args
+      out = HTTParty.get "#{API_URL}/query"
+      JSON.parse( out.response.body )
   end
   
-  it "should accept traveller reports" do
-    save_report 59.0, 17.35
-    result = search 59.0, 17.35
-    result.size.should == 1
+#  it "should accept traveller reports" do
+#    save_report 59.0, 17.35
+#    result = search 59.0, 17.35
+#    result.size.should == 1
+#  end
+
+  it "retrieve known deviation" do
+    deviation = get_deviation 1234  
+    deviation['deviation']['line_number'].should == '4'
   end
+
 end
